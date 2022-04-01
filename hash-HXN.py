d105762b08872cc160d4fd9a676644daa107c5f7
HEXiN @hxn_here  insta <98489657+HXN2@users.noreply.github.com>
2022-04-01 13:29:24 +0300

    Create hash-HXN.py

---
+++ hash-HXN.py
@@ -0,0 +1,239 @@
+import os
+
+try:
+
+    import requests
+
+    import socket as s
+
+    import webbrowser
+
+    from sys import platform
+
+    import colorama
+
+# Please limit your encryption to 25 characters because this is not allowed
+
+# ارجوك ان لا يتجاوز التشفير الخاص بك 25 حرف لان ذلك غير مسموح
+
+except ModuleNotFoundError:
+
+    os.system("pip install requests")
+
+    os.system("pip install socket")
+
+    os.system("pip install webbrowser")
+
+    os.system("pip install sys")
+
+    os.system("pip install platform")
+
+    os.system("pip install colorama")
+
+import webbrowser
+
+url = "https://t.me/HEXiN1K"
+
+webbrowser.open(url)
+
+from sys import platform
+
+from colorama import *
+
+import socket
+
+import requests
+
+name = socket.gethostname()
+
+myHostName = socket.gethostname()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("\n===========================================================")
+
+name = ("""\n                    Hello sir """ +name+Style.RESET_ALL)
+
+print(name)
+
+host = ("""\n                  Your platform is """+ platform+Style.RESET_ALL)
+
+print(host)
+
+api = "https://api.ipify.org/?format=json"
+
+req = requests.get(api)
+
+response = req.json()
+
+a = response["ip"]
+
+IP = ("""\n                Your Local iP is """+Fore.RED+a+Style.RESET_ALL)
+
+print(IP)
+
+v = (Fore.RED+"""\n                    Your iNFO PROXY is\n"""+Style.RESET_ALL)
+
+print(v)
+
+print(s)
+
+print("\n===========================================================")
+
+lib = input(f"""
+
+\n  من صنع هيكسن انستجرام @hxn_here
+
+\n  BY HEXiN INSTA @hxn_here
+
+\n  tarafından yapılmış HEXiN Instagram @hxn_here
+
+\n
+
+===========================================================
+
+\n   من صنع هيكسن تيليجرام @HEXiN 
+
+\n  BY HEXiN TELeGRaM @HEXiN1K
+
+\n  Bir telgraf HEXiN oluşturun @HEXiN1K
+
+\n ===========================================================
+
+ 
+
+ [1] DOWNLOAD LiP & UPDATE . تنزيل وتحديث المكاتب
+
+\n [2] SKiP DOWNLOAD & UPDATE LiP . تخطي تنزيل وتحديث المكاتب 
+
+\n [+] Please Choice >> """)
+
+os.system("clear")
+
+if lib == "1":
+
+    try:
+
+        os.system('pip install requests')
+
+        os.system('pip install webbrowser')
+
+        os.system('pip install socket')
+
+        os.system('pip install sys')  
+
+        os.system('pip install colorama')
+
+        os.system('pip install time')
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        pass
+
+    except:
+
+        pass
+
+else:
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    pass
+
+import webbrowser
+
+import socket
+
+import requests
+
+from colorama import *
+
+import socket
+
+import time as mm
+
+import sys as n
+
+import os
+
+def slow(M):
+
+	for c in M + '\n':		n.stdout.write(c)
+
+		n.stdout.flush()
+
+		mm.sleep(1. / 75)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+bn1 =(Fore.RED+"""
+
+ ...........................................................  
+
+                     _    ___   ___   _
+
+                    | |  | \ \ / / \ | |
+
+                    | |__| |\ V /|  \| |
+
+                    |  __  | > < | . ` |
+
+                    | |  | |/ . \| |\  |
+
+                    |_|  |_/_/ \_\_| \_|
+
+""")
+
+print(bn1)
+
+# Please limit your encryption to 25 characters because this is not allowed
+
+# ارجوك ان لا يتجاوز التشفير الخاص بك 25 حرف لان ذلك غير مسموح
+
+import time as mm
+
+import sys as n
+
+def slow(M):
+
+	for c in M + '\n':
+
+		n.stdout.write(c)
+
+		n.stdout.flush()
+
+		mm.sleep(1. / 75)
+
+slow(Fore.WHITE+""".........................................................
+
+  
+
+  Coded BY HEXiN inStAGrAm : @hxn_here
+
+   
+
+  TeLEGram programmer : @HEXiN1K 
+
+........................................................... 
+
+                                               """)
+
+ 
+
+ctf1 = input (Fore.CYAN+" [+] Enter Your encryption to know type ==> : ")
+
+api = f"https://mr-abood.herokuapp.com/hash/identifier?hash={ctf1}"
+
+req = requests.get(api)
+
+response = req.json()
+
+ 
+
+a = response["Status"]
+
+g = response ["Type"]
+
+print(a)
+
+print(g)
